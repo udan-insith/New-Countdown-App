964cc1e3e33680e7b8c944eaedae14adbe6e59fa
udan-insith <udaninsith23613@gmail.com>
2026-02-11 20:02:32 +0530

    countdown completed

--- app.js
+++ app.js
@@ -26,7 +26,13 @@ const giveaway = document.querySelector('.giveaway')
 const deadline = document.querySelector('.deadline')
 const items = document.querySelectorAll('.deadline-format h4')
 
-let futureDate = new Date(2026, 1, 13, 21, 19, 0)
+let tempDate = new Date()
+let tempYear = tempDate.getFullYear()
+let tempMonth = tempDate.getMonth()
+let tempDay = tempDate.getDate()
+
+// let futureDate = new Date(2026, 1, 13, 21, 19, 0)
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0)
 
 const year = futureDate.getFullYear()
 const hours = futureDate.getHours()
@@ -38,7 +44,7 @@ const date = futureDate.getDate()
 
 const weekday = weekdays[futureDate.getDay()]
 
-giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}`
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`
 
 // future time in ms
 const futureTime = futureDate.getTime()
@@ -80,6 +86,10 @@ function getRemainingTime() {
     item.innerHTML = format(values[index])
   })
 
+  if (t < 0) {
+    clearInteraval(countdown)
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`
+  }
 }
 
 // countdown
